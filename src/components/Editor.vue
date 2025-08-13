b47e9dbb53ffa4068f6e2dc5fbc8f3cbfc14ec70
iranika <7370486+iranika@users.noreply.github.com>
2025-08-14 02:36:18 +0900

    fix sample code

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
+import Editor from 'primevue/editor';
 import { ref } from 'vue';
 
 const value = ref('<p>Type here...</p>');
 </script>
 
 <template>
-  <Button label="Save" icon="pi pi-check" iconPos="right" />
+  <Editor v-model="value" editorStyle="height: 320px" />
 </template>
