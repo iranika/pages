508fbf228434a3173002c77f9acda3443982b4b0
iranika <7370486+iranika@users.noreply.github.com>
2025-08-23 23:01:26 +0900

    fix ブックマークレットBOXを改修

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@
 import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable Vue to support Vue components.
-	integrations: [vue({ appEntrypoint: './src/pages/_app.ts' })],
+    // Enable Vue to support Vue components.
+    integrations: [vue({ appEntrypoint: './src/pages/_app.ts' }), mdx()],
 });
